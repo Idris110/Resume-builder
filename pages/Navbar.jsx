214d10bcffe4307f1c1ae5098301b3a86521f961
Idris110 <ratlamwalaidris110@gmail.com>
2023-03-19 01:39:46 +0530

    lets analyse

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className="">
-      <nav className="px-2 h-[100px] py-5 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="flex px-2 h-[100px] justify-around py-5 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
 
         <div className="container flex flex-wrap items-center justify-content-start mx-auto">
           <a href="#" className="flex items-center mr-4 ml-10">
@@ -11,7 +11,8 @@ function Navbar() {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
           </a>
           <div className="flex md:order-2 ">
-            <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+
+
             <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -42,7 +43,9 @@ function Navbar() {
           </div>
 
 
+
         </div>
+        <button type="button" className="text-white w-[220px] mr-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lets Analyse Resume</button>
       </nav>
     </div>
   )
